a597dd2e7982db6dfa84c8c06e3268c462a1ef50
Henry <henryquoctran@gmail.com>
2025-07-08 15:59:20 +0700

    henrys profile as a test

--- src/profile.jsx
+++ src/profile.jsx
@@ -3,8 +3,26 @@ import "./profile.scss"
 export function Profile() {
   return <div className="profile">
     <div className="left">
-      <div className=""></div>
+      <div className="panel">
+        <div className="name">Henry Quoc Tran</div>
+        <div className="fakenav">Top Page | Blog | Friends</div>
+        <br />
+        <img className="profile-pic" src={require("./profiles/qwook/profile.jpg")} />
+      </div>
     </div>
-    <div className="right"></div>
+    <div className="right">
+      <div className="panel">
+        <div className="title">Blog</div>
+        <div>
+          <p>When I was a young boy, my father took me into the city to join the black parade.</p>
+        </div>
+      </div>
+      <div className="panel">
+        <div className="title">Games</div>
+        <div>
+          Link and screenshot to games here...
+        </div>
+      </div>
+    </div>
   </div>
 }
