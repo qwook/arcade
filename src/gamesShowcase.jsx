f3ed80038a31f67bec3d87e7a4533b295946988d
Henry Tran <henryquoctran@gmail.com>
2025-07-14 18:04:25 +0700

    autoformat

--- src/gamesShowcase.jsx
+++ src/gamesShowcase.jsx
@@ -1,4 +1,10 @@
-import { createContext, useCallback, useContext, useRef, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from "react";
 import { Preview } from "./preview";
 import { Game } from "./game";
 import { ScreenSaver } from "./screensaver";
@@ -9,22 +15,26 @@ import { Router, RouterProvider } from "./router";
 import "./scrollbar/scrollbar.scss";
 import { GAMES, GAMES_LIST } from "./db/games";
 import { Home } from "./home";
+import { MeGank } from "./megank";
 
 window.setFullscreen && window.setFullscreen(true);
 
-function Games({onGameSelect}) {
-  return <div className="games">
-    {GAMES_LIST.map((id) => {
-      return <Game
-        id={id}
-        onGameSelect={onGameSelect}
-      />
-    })}
+function Games({ onGameSelect }) {
+  return (
+    <div className="games">
+      {GAMES_LIST.map((id) => {
+        return <Game id={id} onGameSelect={onGameSelect} />;
+      })}
     </div>
+  );
 }
 
 export default function App() {
-  return <RouterProvider><GamesShowcase /></RouterProvider>
+  return (
+    <RouterProvider>
+      <GamesShowcase />
+    </RouterProvider>
+  );
 }
 
 export const PreviewContext = createContext();
@@ -42,7 +52,6 @@ export function GamesShowcase() {
   const [screen, setScreen] = useState("games");
   const { url, setUrl } = useContext(Router);
 
-
   const onGameSelect = useCallback((previewData) => {
     // setShowPreview(true);
     // setUrl(["games", "test"]);
@@ -52,14 +61,18 @@ export function GamesShowcase() {
 
   return (
     <>
-      <PreviewContext.Provider value={{onGameSelect}}>
+      <PreviewContext.Provider value={{ onGameSelect }}>
         <div className={["games-screen", showPreview ? "blur" : ""].join(" ")}>
           <div className="window">
             <div className="window-inner">
               <div className="title-bar">
                 <img
                   src={require("./assets/ie_icon.png")}
-                  style={{ height: 19, marginRight: 7, verticalAlign: "bottom" }}
+                  style={{
+                    height: 19,
+                    marginRight: 7,
+                    verticalAlign: "bottom",
+                  }}
                 />
                 mẹGank
               </div>
@@ -80,17 +93,21 @@ export function GamesShowcase() {
                 </div>
               </div>
               <div className="body">
-                <MeGank screen={url[0]} setScreen={(screen) => setUrl([screen])}>
+                <MeGank
+                  screen={url[0]}
+                  setScreen={(screen) => setUrl([screen])}
+                  onGameSelect={onGameSelect}
+                >
                   {(() => {
                     if (url[0] === "friends") {
                       if (url[1]) {
-                        return <Profile id={url[1]} />
+                        return <Profile id={url[1]} />;
                       }
-                      return <FriendsList />
+                      return <FriendsList />;
                     } else if (url[0] === "home") {
-                      return <Home />
+                      return <Home />;
                     } else {
-                      return <Games onGameSelect={onGameSelect} />
+                      return <Games onGameSelect={onGameSelect} />;
                     }
                   })()}
                 </MeGank>
@@ -98,99 +115,9 @@ export function GamesShowcase() {
             </div>
           </div>
         </div>
-        <Preview
-          id={previewData}
-          show={showPreview}
-          onCancel={onCancel}
-        />
+        <Preview id={previewData} show={showPreview} onCancel={onCancel} />
         <ScreenSaver />
       </PreviewContext.Provider>
     </>
-  );
-}
-
-function MeGank({ children, screen, setScreen }) {
-  const nav = [
-    {
-      name: "home",
-      text: "Bắt đầu",
-      icon: require("./assets/icons/house.png"),
-    },
-    {
-      name: "games",
-      text: "Games",
-      icon: require("./assets/icons/controller.png"),
-    },
-    {
-      name: "friends",
-      text: "Bạn Bè",
-      icon: require("./assets/icons/user.png"),
-    },
-    // {
-    //   name: "contact",
-    //   text: "Tin nhắn",
-    //   icon: require("./assets/icons/comments.png"),
-    // },
-  ]
-
-  return (
-      <Lag className="megank">
-    {
-      (() => {
-        if (screen === "friends") {
-          return <Lag className="yahoo">
-            <Lag className="center">
-              <Lag className="topbar">
-                <Lag className="mainbar">
-                  <Lag className="logo"></Lag>
-                  <Lag>Xin chao ban! [ EN / VN ]</Lag>
-                </Lag>
-                <Lag className="subbar">
-                  <Lag className="nav">
-                  {
-                    nav.map((item) => {
-                      return <Lag className={item.name === screen ? "item selected" : "item"} onClick={() => setScreen(item.name)}>
-                        <img src={item.icon} />
-                        &nbsp;{item.text}
-                      </Lag>
-                    })
-                  }
-                  </Lag>
-                </Lag>
-              </Lag>
-              {children}
-            </Lag>
-          </Lag>
-        } else {
-          
-       return  (<Lag className="zing"><Lag className="left">
-          <Lag className="logo"></Lag>
-          <Lag className="sidebar">
-            {
-              nav.map((item) => {
-                return <Lag className={item.name === screen ? "selected" : ""} onClick={() => setScreen(item.name)}>
-                  <img src={item.icon} />
-                  &nbsp;&nbsp;{item.text}
-                </Lag>
-              })
-            }
-          </Lag>
-        </Lag>
-        <Lag className="right">
-          <Lag className="topbar">
-            <Lag className="search">
-              <input type="text" placeholder={"Tìm kiếm"} />
-              <Lag className="search-button">
-                <Lag className="search-icon"></Lag>
-              </Lag>
-            </Lag>
-            <Lag>EN / VN</Lag>
-          </Lag>
-          <Lag className="content">{children}</Lag>
-      </Lag></Lag>)
-        }
-      })()
-    }
-    </Lag>
   );
 }
---
+++ src/megank.jsx
@@ -0,0 +1,186 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import { GAMES, GAMES_LIST } from "./db/games";
+import { PROFILES, PROFILES_LIST } from "./db/profiles";
+import { Lag } from "./lag";
+import { search } from "fast-fuzzy";
+import * as _ from "lodash";
+import { Router } from "./router";
+
+const SEARCH_LIST = [
+  ...GAMES_LIST.map((id) => ({
+    id: id,
+    search: GAMES[id].title,
+    type: "game",
+  })),
+  ...PROFILES_LIST.map((id) => ({
+    id: id,
+    search: PROFILES[id].name,
+    type: "profile",
+  })),
+  ...PROFILES_LIST.map((id) => ({
+    id: id,
+    search: id,
+    type: "profile",
+  })),
+];
+
+export function MeGank({ children, screen, setScreen, onGameSelect }) {
+  const nav = [
+    {
+      name: "home",
+      text: "Bắt đầu",
+      icon: require("./assets/icons/house.png"),
+    },
+    {
+      name: "games",
+      text: "Games",
+      icon: require("./assets/icons/controller.png"),
+    },
+    {
+      name: "friends",
+      text: "Bạn Bè",
+      icon: require("./assets/icons/user.png"),
+    },
+    // {
+    //   name: "contact",
+    //   text: "Tin nhắn",
+    //   icon: require("./assets/icons/comments.png"),
+    // },
+  ];
+
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const { url, setUrl } = useContext(Router);
+
+  const hoverCount = useRef();
+  useEffect(() => {
+    if (showSearch) {
+      hoverCount.current = 0;
+    }
+  }, [showSearch]);
+
+  return (
+    <Lag className="megank">
+      {(() => {
+        if (screen === "friends") {
+          return (
+            <Lag className="yahoo">
+              <Lag className="center">
+                <Lag className="topbar">
+                  <Lag className="mainbar">
+                    <Lag className="logo"></Lag>
+                    <Lag>Xin chao ban! [ EN / VN ]</Lag>
+                  </Lag>
+                  <Lag className="subbar">
+                    <Lag className="nav">
+                      {nav.map((item) => {
+                        return (
+                          <Lag
+                            className={
+                              item.name === screen ? "item selected" : "item"
+                            }
+                            onClick={() => setScreen(item.name)}
+                          >
+                            <img src={item.icon} />
+                            &nbsp;{item.text}
+                          </Lag>
+                        );
+                      })}
+                    </Lag>
+                  </Lag>
+                </Lag>
+                {children}
+              </Lag>
+            </Lag>
+          );
+        } else {
+          return (
+            <Lag className="zing">
+              <Lag className="left">
+                <Lag className="logo"></Lag>
+                <Lag className="sidebar">
+                  {nav.map((item) => {
+                    return (
+                      <Lag
+                        className={item.name === screen ? "selected" : ""}
+                        onClick={() => setScreen(item.name)}
+                      >
+                        <img src={item.icon} />
+                        &nbsp;&nbsp;{item.text}
+                      </Lag>
+                    );
+                  })}
+                </Lag>
+              </Lag>
+              <Lag className="right">
+                <Lag className="topbar">
+                  <Lag className="search">
+                    <input
+                      type="text"
+                      placeholder={"Tìm kiếm"}
+                      onFocus={(e) => {
+                        setShowSearch(true);
+                      }}
+                      onBlur={(e) => {
+                        if (hoverCount.current === 1) {
+                          return;
+                        }
+                        setShowSearch(false);
+                      }}
+                      onChange={(e) => {
+                        let results = search(e.target.value, SEARCH_LIST, {
+                          keySelector: (obj) => obj.search,
+                        });
+                        results = _.uniqBy(results, (val) => val.type + val.id);
+                        setSearchResults(results);
+                      }}
+                    />
+                    {showSearch && (
+                      <div
+                        className="search-list"
+                        onMouseOver={() => {
+                          hoverCount.current = 1;
+                        }}
+                        onMouseOut={() => {
+                          hoverCount.current = 0;
+                        }}
+                      >
+                        {searchResults.map((result) => {
+                          let title;
+                          let cb;
+                          if (result.type === "game") {
+                            title = GAMES[result.id].title;
+                            cb = () => {
+                              onGameSelect(result.id);
+                              setShowSearch(false);
+                            };
+                          } else if (result.type === "profile") {
+                            title = PROFILES[result.id].name;
+                            cb = () => {
+                              setUrl(["friends", result.id]);
+                              setShowSearch(false);
+                            };
+                          }
+                          return (
+                            <div onClick={cb} className="search-item">
+                              {title}
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                    <Lag className="search-button">
+                      <Lag className="search-icon"></Lag>
+                    </Lag>
+                  </Lag>
+                  <Lag>EN / VN</Lag>
+                </Lag>
+                <Lag className="content">{children}</Lag>
+              </Lag>
+            </Lag>
+          );
+        }
+      })()}
+    </Lag>
+  );
+}
