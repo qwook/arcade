b92e7388c56a34eddefd5dcee7972dd775814b83
Henry Tran <henryquoctran@gmail.com>
2025-07-18 11:19:18 +0700

    even more profiles

---
+++ src/db/profiles.tsx
@@ -0,0 +1,386 @@
+import { WebContentsDidStartNavigationEventParams } from "electron";
+import { JSX } from "react";
+
+interface Profile {
+  [id: string]: {
+    name: string;
+    avatar: string;
+    games: string[];
+    blurbEN: JSX.Element | string;
+    blurbVN: JSX.Element | string;
+    aboutMeEN: JSX.Element | string;
+    aboutMeVN: JSX.Element | string;
+    theme?: {
+      color1: string;
+      color2: string;
+      color3: string;
+      backgroundImage: string;
+    };
+    location?: string;
+    age?: string;
+    social?: {
+      ig?: string;
+      fb?: string;
+      itch?: string;
+    };
+  };
+}
+
+export const PROFILES: Profile = {
+  PandaBeo04: {
+    name: "Thinh Bui",
+    avatar: require("../profiles/pandabeo/avatar.jpg"),
+    age: "21",
+    location: "Ho Chi Minh City",
+    games: ["agameaboutme", "dfishyfinal"],
+    blurbEN: (
+      <>
+        getting lost in the world of games was such an exciting experience for
+        me and that’s what sparked the passion for gaming I have today
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        My childhood was all about those days I skipped school to play video
+        games. I used to sneak over to my friend’s house to play Contra on his
+        NES, and as I got older, I’d cut class to play League of Legends at a
+        net café near home. Back then, getting lost in the world of games was
+        such an exciting experience for me and that’s what sparked the passion
+        for gaming I have today.
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+    social: {
+      fb: "truongthinh.bui.98",
+      itch: "pandabeo04",
+    },
+  },
+  Ciiverix: {
+    name: "Quyen Nguyen",
+    avatar: require("../profiles/quyennguyen/congchuabanggia.jpg"),
+    games: ["bloodtoothtears", "dfishyfinal", "somethingsfishy"],
+    location: "Ho Chi Minh City",
+    age: "20",
+    blurbEN: (
+      <>
+        I played a rhythm MMOG with graphics way too powerful for my potato PC
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        Back in 2010-something, I played a rhythm MMOG with graphics way too
+        powerful for my potato PC: glitches everywhere, missing textures, and my
+        character was always a purple ghost. So, during the week, I’d save my
+        breakfast money all week just to hit the quán net for a weekend glow-up
+        and shopping spree!
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+    social: {
+      fb: "cvrix",
+      ig: "ciiverix",
+      itch: "quyen-cvrix-nguyen",
+    },
+  },
+  Lin_nguyenn: {
+    name: "Lin_nguyenn",
+    avatar: require("../assets/placeholders/missing.png"),
+    games: ["dfishyfinal"],
+    blurbEN: <>i make games!!</>,
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: "",
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+  LAN4032: {
+    name: "Bui Chi (Ashley) Lan",
+    location: "Ho Chi Minh City",
+    age: "21",
+    avatar: require("../profiles/ashley/avatar.jpg"),
+    games: ["mamapleasewait"],
+    blurbEN: (
+      <>
+        they only had educational games like Spelling Sharks or Hangman, those
+        were the highlights of my day as a primary student
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        <p>
+          <img
+            style={{ width: 300 }}
+            src={require("../profiles/ashley/Hangman.gif")}
+          />
+          <img
+            style={{ width: 300 }}
+            src={require("../profiles/ashley/Spelling Shark.jpg")}
+          />
+        </p>
+        <p>
+          My earliest memory of gaming comes from playing pre-installed
+          applications on my old school’s computer. Though they only had
+          educational games like Spelling Sharks or Hangman, those were the
+          highlights of my day as a primary student.
+        </p>
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+  duyenle: {
+    name: "Duyen Le",
+    location: "Ho Chi Minh City",
+    age: "23",
+    avatar: require("../profiles/duyenle/avatar.jpg"),
+    games: ["mamapleasewait"],
+    blurbEN: (
+      <>
+        the first two games that really stuck with me in 2012-2013 were Khu Vườn
+        Trên Mây and Gunny.
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        <p>
+          <img
+            style={{ width: 300 }}
+            src={require("../profiles/duyenle/khuvuon.png")}
+          />
+          <img
+            style={{ width: 300 }}
+            src={require("../profiles/duyenle/gunny.jpg")}
+          />
+        </p>
+        <p>
+          When I was a child, my parents didn’t allow me to use the computer
+          often, fearing it would harm my eyes and affect my studies. But once I
+          was finally allowed, the first two games that really stuck with me in
+          2012-2013 were Khu Vườn Trên Mây and Gunny. Khu Vườn Trên Mây gave me
+          a sense of peace and simplicity such as planting trees, harvesting,
+          taking care of pets and decorating my own garden. On the other hand,
+          Gunny was interesting and fun. I loved simulating “chicken” , aiming
+          the perfect shot with the cute layout design. Even though there are
+          more modern games now, the excitement I felt back then remains one of
+          my fondest childhood memories.
+        </p>
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+  cam: {
+    name: "Vo Thai Phuong Anh",
+    location: "Ho Chi Minh City",
+    age: "20",
+    avatar: require("../profiles/vothaiphuonganh/avatar.jpg"),
+    games: ["mamapleasewait"],
+    blurbEN: <>y8.com and socnhi.com are my childhood</>,
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        <p>
+          y8.com and socnhi.com are my childhood. They helped a poor kid like me
+          have an opportunity to play video games at that time. So nostalgic ~
+        </p>
+        <p>
+          <img
+            style={{ width: 300 }}
+            src={require("../profiles/vothaiphuonganh/y8.png")}
+          />
+          <img
+            style={{ width: 300 }}
+            src={require("../profiles/vothaiphuonganh/socnhi.png")}
+          />
+        </p>
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+  ArceStarwalker: {
+    name: "Maple Nguyen",
+    location: "Hanoi",
+    age: "26 bánh chýng pot",
+    avatar: require("../profiles/Maple.jpg"),
+    games: ["firstprinciple"],
+    blurbEN: (
+      <>
+        eavesdropping and secretly watching my parent as they type the
+        computer's password, JUST to play games
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        I was probably the wildest card anybody has known, breaking through
+        computer's security with ease and going as far as eavesdropping and
+        secretly watching my parent as they type the computer's password, JUST
+        to play games. But like, games were so funnnnn, especially those typing
+        games and time management games. Nobody, and I meant NOBODY could beat
+        me in speed.
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+    social: {
+      fb: "Maplesayer",
+      ig: "arcenguyen",
+    },
+  },
+  theguyser: {
+    name: "Bao",
+    age: "26",
+    location: "Ho Chi Minh City",
+    avatar: require("../profiles/baochua/avatar.png"),
+    games: ["mineblast"],
+    blurbEN: (
+      <>
+        It was a blast as getting to share these experiences with others,
+        especially since the games were meant to be played in a group
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        <p>
+          During the Wii and Wii U era, I would invite friends and family to
+          play games such as Wii Sports, Nintendo Land and Super Smash Bros. I
+          acquired both my Wii and Wii U from game stores on Pasteur Street, Ho
+          Chi Minh City. Halo Shop which is still here today and Mimigames which
+          moved to another location in the city though they both no longer sell
+          bootleg games. It was a blast as getting to share these experiences
+          with others, especially since the games were meant to be played in a
+          group, which shaped my nostalgia and enjoyment of Nintendo games. I am
+          nostalgic for those days as in-person gaming has become less popular
+          and harder to set up as everyone is busier or no longer nearby.
+        </p>
+        <p>
+          My first experience with video games was from school. In Kindergarten,
+          I was introduced to educational games on the web. I was hooked
+          immediately and asked how to access them at home though wasn't told
+          how. This would lead into my experiences messing with the family
+          computer by surfing the early 2000s web, leading to countless viruses
+          but also my love for computers and gaming. When I was a little older,
+          my first experiences with online games would be with Audition Online
+          and Boom Online. I wasn't very good at these games but enjoyed playing
+          with random players online.
+        </p>
+        <p>
+          I started getting interested in making video games when I was taught
+          Scratch in a computer class in Middle School. Some Scratch games I
+          made were a Pac Man clone, a horizontal SHMUP and a Frogger clone. I
+          didn't make any games in High School but learned Python and Java in my
+          computer science classes. I made a text adventure in Python and tried
+          to make a Fire Emblem clone using Java but wasn't able to figure out
+          rendering graphics and gave up on the project. However, the experience
+          I had with these 2 programming languages made C# for Unity easier to
+          pick up.
+        </p>
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+  watehmeloan: {
+    name: "Nhu Vu",
+    avatar: require("../profiles/nhuvu/it_me.png"),
+    games: ["mineblast"],
+    blurbEN: (
+      <>
+        {" "}
+        I had a knack for making my own game interpretations [...] using paper
+        and clay
+      </>
+    ),
+    blurbVN: <>tui lam game!!</>,
+    aboutMeEN: (
+      <>
+        I have an odd relationship with games ever since I was little, I didn't
+        understand how computer games worked but I had a knack for making my own
+        game interpretations from them using paper and clay. Not sure how that
+        path set me off to playing and making games in pixel art now but it is
+        what it is
+      </>
+    ),
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+  qwook: {
+    name: "Henry Quoc Tran",
+    avatar: require("../profiles/qwook/profile.jpg"),
+    games: [],
+    blurbEN: (
+      <>
+        i'm dancing <img src={require("../assets/emojis/4.gif")} /> and
+        crocheting~~~
+      </>
+    ),
+    blurbVN: (
+      <>
+        i'm dancing <img src={require("../assets/emojis/4.gif")} /> and
+        crocheting~~~
+      </>
+    ),
+    aboutMeEN: "",
+    aboutMeVN: "",
+    theme: {
+      color1: "",
+      color2: "",
+      color3: "",
+      backgroundImage: "",
+    },
+  },
+};
+
+export const PROFILES_LIST = Object.keys(PROFILES);
--- src/friendlist.jsx
+++ src/friendlist.jsx
@@ -1,40 +1,80 @@
-import { PROFILES, PROFILES_LIST } from "./db/profiles"
-import "./friendlist.scss"
-import { Lag } from "./lag"
+import { useState } from "react";
+import { PROFILES, PROFILES_LIST } from "./db/profiles";
+import "./friendlist.scss";
+import { Lag } from "./lag";
 import { Link } from "./router";
 
 export function FriendsList() {
-  return <div className="friends">
-    <Lag className="panel friend-list">
-      <Lag className="heading">Bai moi nhat</Lag>
-      {
-        PROFILES_LIST.map((id) => {
+  const [funFact, setFunFact] = useState(0);
+  const funFacts = [
+    <>
+      60% of Vietnam's internet is connected from Vũng Tàu to California by a
+      20,000 KM underwater cable.
+    </>,
+    <>
+      The first female video game protagonist was created by Vietnamese
+      programmer Van Mai.
+    </>,
+    <>
+      The Phong shader, developed by Bui Tuong Phong, was essential to rendering
+      3D graphics – especially on the PS2.
+    </>,
+    <>
+      Flappy Bird was the first the highest selling game made by a Vietnamese
+      developer.
+    </>,
+    <>
+      Counter-Strike was co-created as a Half-Life mod by Lê "Gooseman" Minh.
+    </>,
+  ];
+
+  return (
+    <div className="friends">
+      <Lag className="panel friend-list">
+        <Lag className="heading">Bai moi nhat</Lag>
+        {PROFILES_LIST.map((id) => {
           const profile = PROFILES[id];
           return (
             <Link className="friend" href={["friends", id]}>
               <Lag className="person">
                 <Lag className="avatar-wrapper">
-                  <Lag className="avatar" style={{backgroundImage: `url('${profile.avatar}')`}} />
+                  <Lag
+                    className="avatar"
+                    style={{ backgroundImage: `url('${profile.avatar}')` }}
+                  />
                 </Lag>
-                <Lag className="name">
-                  {profile.name}
-                </Lag>
+                <Lag className="name">{profile.name}</Lag>
               </Lag>
               <Lag className="quote">{profile.blurbEN}</Lag>
             </Link>
-        )
-      })
-    }
-    </Lag>
-    <Lag className="panel" style={{width: 250}}>
-      <Lag className="welcome">
-        <Lag className="heading2">Fun Facts!</Lag>
-        <Lag className="avatar">
-          <Lag className="bubble"><Lag className="bubble-inner">I don't know anything. Sorry!</Lag></Lag>
+          );
+        })}
+      </Lag>
+      <Lag className="panel" style={{ width: 250 }}>
+        <Lag className="welcome">
+          <Lag className="heading2">Fun Facts!</Lag>
+          <Lag className="avatar">
+            <Lag className="bubble">
+              <Lag className="bubble-inner">
+                {funFacts[funFact % funFacts.length]}
+                <br />
+                <br />
+                <div
+                  className="link"
+                  onClick={(e) => {
+                    setFunFact((funFact) => (funFact + 1) % funFacts.length);
+                  }}
+                >
+                  Next ►
+                </div>
+              </Lag>
+            </Lag>
+          </Lag>
         </Lag>
       </Lag>
-    </Lag>
-    <style dangerouslySetInnerHTML={{__html: `
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
       .megank .yahoo {
         background-image: url('${require("./assets/placeholders/bg_swirl.jpg")}');
         background-size: cover;
@@ -42,6 +82,9 @@ export function FriendsList() {
       .games-screen .body {
         background-color: #af3582;
       }
-    `}} />
-  </div>
+    `,
+        }}
+      />
+    </div>
+  );
 }
